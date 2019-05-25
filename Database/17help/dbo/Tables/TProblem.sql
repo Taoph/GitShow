585ada8c875f9b573d8b35fd1019505872206ff8
Taoph <2840390273@qq.com>
2019-05-25 17:09:18 +0800

    20190525:bootstrap修改,添加数据库

---
+++ Database/17help/dbo/Tables/TProblem.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TProblem] (
+    [ID]              INT            IDENTITY (1, 1) NOT NULL,
+    [Title]           NVARCHAR (255) NOT NULL,
+    [Content]         TEXT           NOT NULL,
+    [NeedRemoteHelp]  BIT            NULL,
+    [Reward]          INT            NOT NULL,
+    [PublishDateTime] DATETIME       NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Database/17help/dbo/Tables/TUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TUser] (
+    [ID]         INT          IDENTITY (1, 1) NOT NULL,
+    [UserName]   VARCHAR (30) NOT NULL,
+    [Password]   VARCHAR (50) NOT NULL,
+    [Inviter]    VARCHAR (30) NOT NULL,
+    [InviteCode] CHAR (6)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
