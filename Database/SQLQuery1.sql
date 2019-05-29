65dfa54b9c6bd623098ffc53a5ddeb87f4f52059
Taoph <2840390273@qq.com>
2019-05-29 22:57:22 +0800

    20190529：添加约束

---
+++ Database/SQLQuery1.sql
@@ -0,0 +1,211 @@
+﻿/*
+17help 的部署脚本
+
+此代码由工具生成。
+如果重新生成此代码，则对此文件的更改可能导致
+不正确的行为并将丢失。
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "17help"
+:setvar DefaultFilePrefix "17help"
+:setvar DefaultDataPath "C:\Users\asus\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\ProjectsV13\"
+:setvar DefaultLogPath "C:\Users\asus\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\ProjectsV13\"
+
+GO
+:on error exit
+GO
+/*
+请检测 SQLCMD 模式，如果不支持 SQLCMD 模式，请禁用脚本执行。
+要在启用 SQLCMD 模式后重新启用脚本，请执行:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'要成功执行此脚本，必须启用 SQLCMD 模式。';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+
+IF (SELECT OBJECT_ID('tempdb..#tmpErrors')) IS NOT NULL DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'正在创建 [dbo].[Table]...';
+
+
+GO
+CREATE TABLE [dbo].[Table] (
+    [Id]         INT          NOT NULL,
+    [UserName]   VARCHAR (20) NOT NULL,
+    [Password]   VARCHAR (50) NOT NULL,
+    [Inviter]    VARCHAR (20) NOT NULL,
+    [InviteCode] CHAR (6)     NOT NULL,
+    [Age]        INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([UserName] ASC)
+);
+
+
+GO
+IF @@ERROR <> 0
+   AND @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK;
+    END
+
+IF @@TRANCOUNT = 0
+    BEGIN
+        INSERT  INTO #tmpErrors (Error)
+        VALUES                 (1);
+        BEGIN TRANSACTION;
+    END
+
+
+GO
+PRINT N'正在创建 [dbo].[Table] 上未命名的约束...';
+
+
+GO
+ALTER TABLE [dbo].[Table] WITH NOCHECK
+    ADD CHECK (Age>0 and Age<200);
+
+
+GO
+IF @@ERROR <> 0
+   AND @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK;
+    END
+
+IF @@TRANCOUNT = 0
+    BEGIN
+        INSERT  INTO #tmpErrors (Error)
+        VALUES                 (1);
+        BEGIN TRANSACTION;
+    END
+
+
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT N'数据库更新的事务处理部分成功。'
+COMMIT TRANSACTION
+END
+ELSE PRINT N'数据库更新的事务处理部分失败。'
+GO
+DROP TABLE #tmpErrors
+GO
+PRINT N'根据新创建的约束检查现有的数据';
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+CREATE TABLE [#__checkStatus] (
+    id           INT            IDENTITY (1, 1) PRIMARY KEY CLUSTERED,
+    [Schema]     NVARCHAR (256),
+    [Table]      NVARCHAR (256),
+    [Constraint] NVARCHAR (256)
+);
+
+SET NOCOUNT ON;
+
+DECLARE tableconstraintnames CURSOR LOCAL FORWARD_ONLY
+    FOR SELECT SCHEMA_NAME([schema_id]),
+               OBJECT_NAME([parent_object_id]),
+               [name],
+               0
+        FROM   [sys].[objects]
+        WHERE  [parent_object_id] IN (OBJECT_ID(N'dbo.Table'))
+               AND [type] IN (N'F', N'C')
+                   AND [object_id] IN (SELECT [object_id]
+                                       FROM   [sys].[check_constraints]
+                                       WHERE  [is_not_trusted] <> 0
+                                              AND [is_disabled] = 0
+                                       UNION
+                                       SELECT [object_id]
+                                       FROM   [sys].[foreign_keys]
+                                       WHERE  [is_not_trusted] <> 0
+                                              AND [is_disabled] = 0);
+
+DECLARE @schemaname AS NVARCHAR (256);
+
+DECLARE @tablename AS NVARCHAR (256);
+
+DECLARE @checkname AS NVARCHAR (256);
+
+DECLARE @is_not_trusted AS INT;
+
+DECLARE @statement AS NVARCHAR (1024);
+
+BEGIN TRY
+    OPEN tableconstraintnames;
+    FETCH tableconstraintnames INTO @schemaname, @tablename, @checkname, @is_not_trusted;
+    WHILE @@fetch_status = 0
+        BEGIN
+            PRINT N'检查约束:' + @checkname + N' [' + @schemaname + N'].[' + @tablename + N']';
+            SET @statement = N'ALTER TABLE [' + @schemaname + N'].[' + @tablename + N'] WITH ' + CASE @is_not_trusted WHEN 0 THEN N'CHECK' ELSE N'NOCHECK' END + N' CHECK CONSTRAINT [' + @checkname + N']';
+            BEGIN TRY
+                EXECUTE [sp_executesql] @statement;
+            END TRY
+            BEGIN CATCH
+                INSERT  [#__checkStatus] ([Schema], [Table], [Constraint])
+                VALUES                  (@schemaname, @tablename, @checkname);
+            END CATCH
+            FETCH tableconstraintnames INTO @schemaname, @tablename, @checkname, @is_not_trusted;
+        END
+END TRY
+BEGIN CATCH
+    PRINT ERROR_MESSAGE();
+END CATCH
+
+IF CURSOR_STATUS(N'LOCAL', N'tableconstraintnames') >= 0
+    CLOSE tableconstraintnames;
+
+IF CURSOR_STATUS(N'LOCAL', N'tableconstraintnames') = -1
+    DEALLOCATE tableconstraintnames;
+
+SELECT N'约束验证失败:' + [Schema] + N'.' + [Table] + N',' + [Constraint]
+FROM   [#__checkStatus];
+
+IF @@ROWCOUNT > 0
+    BEGIN
+        DROP TABLE [#__checkStatus];
+        RAISERROR (N'验证约束时出错', 16, 127);
+    END
+
+SET NOCOUNT OFF;
+
+DROP TABLE [#__checkStatus];
+
+
+GO
+PRINT N'更新完成。';
+
+
+GO
---
+++ Database/dbo.TUser.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[TUser] ([Id], [UserName], [Password], [Inviter], [InviteCode], [Age]) VALUES (1, N'qww', N'qwee', N'qeee', N'qwee  ', 100)
+INSERT INTO [dbo].[TUser] ([Id], [UserName], [Password], [Inviter], [InviteCode], [Age]) VALUES (2, N'qwwa', N'dfff', N'aff', N'adff  ', 55)
