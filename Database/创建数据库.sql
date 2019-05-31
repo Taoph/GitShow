e3ea9df204508a57c48624c4e3fca50a16b8615d
Taoph <2840390273@qq.com>
2019-05-31 23:19:39 +0800

    20190531：数据库删除更新

---
+++ Database/创建数据库.sql
@@ -0,0 +1,55 @@
+﻿use [17help] 
+CREATE TABLE [dbo].[TProblem] (
+    [ID]              INT            IDENTITY (1, 1) NOT NULL,
+    [Title]           NVARCHAR (255) NOT NULL,
+    [Content]         TEXT           NOT NULL,
+    [NeedRemoteHelp]  BIT            NULL,
+    [Reward]          INT            NOT NULL,
+    [PublishDateTime] DATETIME       NOT NULL,
+    [AuthorID]        INT            NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_TProblem_TUser] FOREIGN KEY ([AuthorID]) REFERENCES [dbo].[TUser] ([ID])
+);
+CREATE TABLE [dbo].[TRegister] (
+    [ID]       INT          IDENTITY (1, 1) NOT NULL,
+    [UserName] VARCHAR (30) NOT NULL,
+    [Password] VARCHAR (50) NOT NULL,
+    [InviteID] INT          NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_TUser_TUser] FOREIGN KEY ([InviteID]) REFERENCES [dbo].[TRegister] ([ID])
+);
+
+Create table TProfile
+ (
+    ID  int Primary key not null,
+    HeadSculpture image null,
+	Gender nchar(1) check(Gender=N'男' or Gender=N'女'),
+	Birthday datetime null,
+	Keywords varchar(100) null,
+	SelfIntroduction text null,
+	Constraint FK_TProfile Foreign Key (ID) references TRegister(ID)
+ )
+
+ Create table TKeyword
+ (
+    ID int primary key identity not null,
+	Keyword varchar(50) unique not null,
+ )
+ 
+ Create table ProblemKeyword
+ (
+   ID int primary key identity not null,
+   ProblemID int,
+   KeywordID int,
+   constraint FK_ProblemKeyword_Problem Foreign Key (ProblemID) references TProblem(ID),
+   constraint FK_ProblemKeyword_Keywoed Foreign Key (KeywordID) references TKeyword(ID)
+ )
+
+ Create table TCredit 
+ (
+   ID int primary key identity,
+   Method varchar(50) not null,
+   Value int not null,
+ )
+ alter  table TCredit 
+ add constraint UQ_TCredit_Method unique(Method)
---
+++ Database/删除更新sql.sql
@@ -0,0 +1,45 @@
+﻿Insert into TCredit values(N'督促',2),('评价分',1)
+
+alter table TCredit drop UQ_TCredit_Method
+
+Insert into TCredit values('督促',2),('评价',1),('其它',15),('邮箱注册',20),('注册',10)
+
+delete TCredit 
+
+truncate table TCredit
+
+drop table TCredit
+
+Create table TCredit
+(
+   ID int Primary key identity,
+   CreateTime datetime,
+   kind   NVarchar(10),
+   Value  int,
+   Total  int,
+   Introduction NVarchar(100),
+   UserID  int Foreign key references TRegister(ID)
+)
+
+alter table TProblem
+add constraint CK_TProblem_Reward check(Reward>0)
+
+Insert into TProblem(Title,Content,PublishDateTime,Reward) values('求助1','内容1',getDate(),3.5)
+
+Select * from TProblem
+
+alter table TProblem drop CK_TProblem_Reward
+
+insert into TProblem(Title,Content,PublishDateTime,Reward) values('求助2','内容2',getDate(),-5),
+('求助3','内容3',getDate(),-1),('求助4','内容4',getDate(),-3),('求助5','内容5',getDate(),2)
+
+
+
+alter table TProblem add Body text
+update TProblem set Body=Content 
+alter table TProblem drop column Content
+
+insert into TProblem(Title,Body,PublishDateTime,Reward) values('求助2','内容2',getDate(),-5),
+('求助3','内容3',getDate(),-1),('求助4','内容4',getDate(),-3),('求助5','内容5',getDate(),2)
+
+update  TProblem set Reward=Reward+1 where abs(Reward)%2=1
