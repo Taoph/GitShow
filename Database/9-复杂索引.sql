c6f6add45911fd788b2c1fad2a78de603c8d42cc
Taoph <2840390273@qq.com>
2019-06-21 17:17:16 +0800

    20190621：复杂索引

---
+++ Database/9-复杂索引.sql
@@ -0,0 +1,40 @@
+﻿insert into  TAd
+select [Name],Title,Cost
+from TAd
+
+drop table TAd
+
+Create table TAd
+(
+  ID int identity,
+  Name varchar(50),
+  Cost int
+)
+
+insert into TAd values('liili',50),('migg',25),('ligfs',43)
+
+
+checkpoint
+DBCC DROPCLEANBUFFERS
+
+SET Statistics io on
+insert into TAd
+select Name,Cost
+from TAd
+set statistics io off
+
+set statistics io on
+select ID
+from TAd
+where ID>25
+set statistics io off
+
+create clustered index IX_TAd_ID on TAd(ID)
+drop index TAd.IX_TAd_ID
+
+create index IX_TAd_ID on TAd(ID,Cost)
+drop index TAd.IX_TAd_ID
+create unique index IX_TAd_ID on TAd(ID)
+
+backup database [17help] to disk='F:\GitShow\Database\17help.db'
+
