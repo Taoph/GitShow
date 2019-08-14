befdb474bf8f1b235ab1d83b4d9414a728a9bcc6
Taoph <2840390273@qq.com>
2019-08-14 19:20:34 +0800

    JAVAScript作业入门1

---
+++ Database/10-表表达式和tempdb.sql
@@ -0,0 +1,15 @@
+﻿
+
+
+select 
+(select  P.ID ,P.AuthorID,count(*) as count
+from TProblem P join ProblemKeyword PK on P.ID=PK.ProblemID
+group by P.ID,P.AuthorID) as p1
+join
+(select AuthorID from TProblem
+group by AuthorID) as p2
+on p1.AuthorID=p2.AuthorID
+
+
+
+
